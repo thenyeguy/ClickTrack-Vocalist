2c3bf9befeb595e03ab1373769fac77c6ef6f1f4
Michael Nye <mnye@andrew.cmu.edu>
2013-06-13 23:37:01 -0700

    Erryday I'm documenting

--- src/filterblock.h
+++ src/filterblock.h
@@ -4,24 +4,38 @@
 #include "ringbuffer.h"
 
 
-class FilterBlock
-{
-    protected:
-        SAMPLE* in_buffer;
-    public:
-        RingBuffer<SAMPLE> out_buffer;
+namespace filterblocks {
+    /* A filter block is the generic building block for a signal processing
+     * chain. All real filters inherit from this interface.
+     *
+     * To be a valid filter block, a filter must adhere to two properties:
+     *   (1) on fill_in_buffer, it must populate its input buffer from the
+     *       previous element in the chain. An object may make up its input if
+     *       it is the first element (ie a microphone input).
+     *   (2) on fill_out_buffer, it must use the in_buffer to calculate the
+     *       output of the filter, and store it in the output RingBuffer so that
+     *       the next element in the chain can access it. If this is the last
+     *       element, it may just throw into space (ie a speaker output).
+     */
+    class FilterBlock
+    {
+        protected:
+            SAMPLE* in_buffer;
+        public:
+            RingBuffer<SAMPLE> out_buffer;
 
-        virtual void fill_in_buffer(RingBuffer<SAMPLE>* input)
-        {
-            int i = 0;
-            while(!input->is_empty())
+            virtual void fill_in_buffer(RingBuffer<SAMPLE>* input)
             {
-                in_buffer[i] = input.deq();
-                i++;
+                int i = 0;
+                while(!input->is_empty())
+                {
+                    in_buffer[i] = input.deq();
+                    i++;
+                }
             }
-        }
 
-        virtual void fill_out_buffer() = 0;
+            virtual void fill_out_buffer() = 0;
+    }
 }
 
 
--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -5,6 +5,9 @@
 
 
 namespace ringbuffer {
+    /* Define two exceptions for use in RingBuffer. Thrown when a ring buffer is
+     * required to be nonempty or nonfull.
+     */
     class EmptyRingBufferException: public std::exception
     {
         virtual const char* what() const throw()
@@ -12,8 +15,6 @@ namespace ringbuffer {
             return "Can't dequeue from an empty buffer";
         }
     };
-
-
     class FullRingBufferException: public std::exception
     {
         virtual const char* what() const throw()
@@ -23,18 +24,27 @@ namespace ringbuffer {
     };
 
     
+    /* Ringbuffer is a template for a generic circular buffer queue. It contains
+     * a single statically sized ring, and it can seamlessly wrap around the
+     * edge. It appears to be of infinite length as long as you never have more
+     * than buffer_size items inside.
+     */
     template <class SampleT>
     class RingBuffer
     {
         private:
-            int start;
-            int end;
-            int size;
+            int start; // starting index of the queue in the ring
+            int end;   // ending index of the queue in the ring
+            int size;  // the number of elements contained in the ring
 
-            int buffer_size;
-            SampleT* samples;
+            int buffer_size;  // the max size of the ring
+            SampleT* samples; // the actual ring array
         
         public:
+            /* The constructor allocates the ring array. If no size is
+             * specified, it can contain at most 1024 elements, otherwise it can
+             * contain as many as its argument allows.
+             */
             RingBuffer(int n_buffer_size=1024)
             {
                 start = 0;
@@ -45,23 +55,31 @@ namespace ringbuffer {
                 samples = new SampleT[buffer_size];
             }
 
+            /* Deconstructor. Frees the memory allocated to the ring.
+             */
             ~RingBuffer()
             {
                 delete samples;
             }
 
-
+            /* Returns true iff there are currently no elements in the ring.
+             */
             bool is_empty()
             {
                 return size == 0;
             }
 
+            /* Returns the number of elements in the ring that have been
+             * enqueued but not yet dequeued.
+             */
             int get_size()
             {
                 return size;
             }
 
-
+            /* Enqueue takes a single sample and puts it at the end of the ring.
+             * If the ring is already full, throws FullRingBufferException.
+             */
             void enq(SampleT s)
             {
                 if(size == buffer_size)
@@ -72,6 +90,10 @@ namespace ringbuffer {
                 size++;
             }
 
+            /* Dequeue removes a single sample from the beginning of the ring.
+             * After a dequeue, the head of the ring is advanced by one.
+             * If the ring is already empty, throws EmptyRingBufferException
+             */
             SampleT deq()
             {
                 if(size == 0)
