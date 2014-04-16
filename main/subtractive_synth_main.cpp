5a9c64397d4711d7e70c83366e46076ce4e86b4f
Michael Nye <thenyeguy@gmail.com>
2014-04-16 01:01:31 -0400

    Added limiter to subsynth

--- main/subtractive_synth_main.cpp
+++ main/subtractive_synth_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../src/clip_detector.h"
+#include "../src/limiter.h"
 #include "../src/midi_wrapper.h"
 #include "../src/speaker.h"
 #include "../src/subtractive_synth.h"
@@ -13,13 +14,15 @@ int main()
     MidiListener midi(&inst, 1);
 
     inst.filter.set_cutoff(1000);
-
     inst.set_lfo_vibrato(0.1);
     inst.set_lfo_tremelo(.2);
 
     cout << "Initializing signal chain" << endl;
+    Limiter limiter(-3.0);
+    limiter.set_input_channel(inst.get_output_channel());
+
     ClipDetector clip(1.0);
-    clip.set_input_channel(inst.get_output_channel());
+    clip.set_input_channel(limiter.get_output_channel());
 
     Speaker out;
     out.set_input_channel(clip.get_output_channel());
